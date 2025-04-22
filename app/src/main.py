ae03deba3a789a797b642f041ffc0c4faed4539f
rio <75190035+HyoJongPark@users.noreply.github.com>
2025-04-22 09:45:01 +0900

    feat: fast api 설정 추가
    - db 세션 미들웨어 추가
    - 사용자 라우터 추가

---
+++ app/src/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from app.src.config.database import Base, engine
+from app.src.middleware.db_transaction import DBSessionMiddleware
+from app.src.user.router import router as user_router
+
+# initialize fastapi
+app = FastAPI()
+app.include_router(user_router)
+
+app.add_middleware(DBSessionMiddleware)
+
+# initialize database
+Base.metadata.create_all(engine)
+
+
+@app.get("/")
+async def warmup():
+    ## 초기 데이터 생성
+    return {"message": "Hello World"}
