e7b8f14d39835dc1275e4f6da4ba0ad0ed41969b
rio <75190035+HyoJongPark@users.noreply.github.com>
2025-04-22 09:38:45 +0900

    feat: 회원가입/로그인 엔드포인트 추가

---
+++ app/src/user/router.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+from app.src.common.token import Token
+from app.src.config.database import get_db_from_request
+from app.src.user.dto.request.user_create_request import UserCreateRequest
+from app.src.user.dto.response.user_create_response import UserCreateResponse
+from app.src.user import service as user_service
+
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+
+@router.post("/register", status_code=status.HTTP_201_CREATED)
+def register(
+    user: UserCreateRequest, db: Session = Depends(get_db_from_request)
+) -> UserCreateResponse:
+    return user_service.register(db, user)
+
+
+@router.post("/login")
+def login(
+    email: str, password: str, db: Session = Depends(get_db_from_request)
+) -> Token:
+    return user_service.login(db, email, password)
