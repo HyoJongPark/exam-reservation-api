f4c20ad7a414adf3ddd18d8c72fa60f71588222d
rio <75190035+HyoJongPark@users.noreply.github.com>
2025-04-23 20:07:04 +0900

    chore: 사용자 생성 시 role을 default로 사용하지 않도록 수정

--- app/src/user/dto/request/user_create_request.py
+++ app/src/user/dto/request/user_create_request.py
@@ -7,9 +7,7 @@ class UserCreateRequest(BaseModel):
     email: str = Field(..., description="이메일", example="test@test.com")
     password: str = Field(..., description="비밀번호", example="password")
     user_name: str = Field(..., description="이름", example="홍길동")
-    role: Role = Field(
-        default=Role.USER, description="계정 권한(user or admin)", example=Role.ADMIN
-    )
+    role: Role = Field(description="계정 권한(user or admin)", example=Role.ADMIN)
 
     def toModel(self) -> User:
         return User(
